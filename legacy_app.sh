301806c2f027372d55d2ccd2fa0a1aecf237e03e
Manoj Kumar Seti <mnjst36@gmail.com>
2026-02-05 09:59:54 +0530

    Intial legacy HP-UX style application

---
+++ legacy_app.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+LOG_FILE="/var/log/legacy_app.log"
+
+echo "Legacy HP-UX style application running" >> $LOG_FILE
+date >> $LOG_FILE
+echo "------------------------------------" >> $LOG_FILE
+
+
+
+
+
